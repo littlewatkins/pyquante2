f139295f40df714f4f2dd41e363833fd91c53786
littlewatkins <35573848+littlewatkins@users.noreply.github.com>
2021-07-29 16:36:49 -0500

    add docstrings to functions

--- pyquante2/basis/pgbf.py
+++ pyquante2/basis/pgbf.py
@@ -28,6 +28,17 @@ class pgbf(object):
     """
     contracted = False
     def __init__(self,exponent,origin=(0,0,0),powers=(0,0,0)):
+        """
+        Parameters
+        ----------
+        exponent : int, float
+            the exponent to be used the PGBF amplitude,  
+            the mesh 
+        origin : tuple, optional
+            location in cartesian coordinates, by default (0,0,0)
+        powers : tuple, optional
+            exponents used for gaussian, by default (0,0,0)
+        """
         self.norm = 1
         assert len(origin) == 3
         assert len(powers) == 3
@@ -36,18 +47,40 @@ class pgbf(object):
         self.powers = powers
         self._normalize()
 
-    def __repr__(self): return "pgbf(%f,%s,%s)" % (self.exponent,tuple(self.origin),self.powers)
+    def __repr__(self):
+        """
+        Returns
+        -------
+        str
+        """
+        return "pgbf(%f,%s,%s)" % (self.exponent,tuple(self.origin),self.powers)
 
     def __call__(self,x,y,z):
-        "Compute the amplitude of the PGBF at point x,y,z"
+        """Compute the amplitude of the PGBF at point x,y,z
+        
+        Parameters
+        ----------
+        x : float
+
+        y : float
+
+        z : float
+
+        Returns
+        -------
+        float
+        """
         I,J,K = self.powers
         dx,dy,dz = x-self.origin[0],y-self.origin[1],z-self.origin[2]
         d2 = dx**2 + dy**2 + dz**2
         return self.norm*dx**I*dy**J*dz**K*np.exp(-self.exponent*d2)
 
     def mesh(self,xyzs):
-        """
-        Evaluate basis function on a mesh of points *xyz*.
+        """Evaluate basis function on a mesh of points *xyz*.
+
+        Returns
+        -------
+        float
         """
         I,J,K = self.powers
         d = np.asarray(xyzs,'d')-self.origin
@@ -57,7 +90,8 @@ class pgbf(object):
         return self.norm*d[:,0]**I*d[:,1]**J*d[:,2]**K*np.exp(-self.exponent*d2)
 
     def _normalize(self):
-        "Normalize basis function. From THO eq. 2.2"
+        """Normalize basis function. From THO eq. 2.2
+        """
         l,m,n = self.powers
         self.norm = np.sqrt(pow(2,2*(l+m+n)+1.5)*
                             pow(self.exponent,l+m+n+1.5)/
